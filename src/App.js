268f2ab8287d72b1563bfe3d346029bb90c69c28
Waqas nazir <vickydev31113@gmail.com>
2021-06-02 02:12:04 +0500

    eslint added and configured & gitignore added and configured as well

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ const Pet = (props) => {
     React.createElement("h3", {}, props.breed),
   ]);
 };
+
+const x = 5
+
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
