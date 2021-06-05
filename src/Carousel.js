25d10c5f1531e9895a23e409ff1a5f5fc453485f
Waqas nazir <vickydev31113@gmail.com>
2021-06-06 01:27:31 +0500

    managed state in class component

---
+++ src/Carousel.js
@@ -0,0 +1,36 @@
+import { Component } from 'react'
+
+class Carousel extends Component {
+    state = {
+        active: 0
+    }
+
+    static defaultProps = {
+        images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+    }
+
+
+    render() {
+        const { active } = this.state;
+        const { images } = this.props
+        return (
+            <div className="carousel">
+                <img src={images[active]} alt='animal' />
+                <div className='carousel-smaller'>
+                    {
+                        images.map((photo, index) => (
+                            <img
+                                key={photo}
+                                src={photo}
+                                alt='animal thumbnail'
+                                className={index === 'active' ? 'active' : ''}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Carousel
--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,12 @@
 import { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import Carousel from './Carousel'
 class Details extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            loading: true
+
+    state = {
+        loading: true
         }
-    }
+
 
     async componentDidMount() {
         const res = await fetch(
@@ -20,9 +20,10 @@ class Details extends Component {
         if (this.state.loading) {
             return <h2>Loading...</h2>
         }
-        const { name, animal, breed, city, state, description, } = this.state;
+        const { name, animal, breed, city, state, description, images } = this.state;
         return (
             <div className='details'>
+                <Carousel images={images} />
                 <div>
                     <h1>{name}</h1>
                     <h2>{`${animal} - ${breed} - ${city} - ${state}`}</h2>
